dc91af8870d7697970fdd4fc3b168a2c921d7e99
Aksh <aksh187k@gmail.com>
2026-05-08 23:35:02 -0400

    Learned Variables

---
+++ Variables.py
@@ -0,0 +1,28 @@
+import ate  # Variables is a container of value (String, integer, float, boolean)
+   # variable acts as if it is the value it contains
+
+   # Strings
+first_name = "Aksh"
+fav_food = "Pizza"
+print(f"{first_name}'s favourite food is {fav_food}.")
+
+# Integer
+age = 19  #Shouldn't put quotes otherwise it will be a string
+quantity = 4
+print(f"I am {age} years old.")
+print(f"I have {quantity} dollars.")
+
+# Float
+price = 10.99
+print(f"one can of coke is ${price}.")
+gpa = 3.7
+print(f"my gpa : {gpa}")
+
+# Boolean
+he_ate = True
+
+if he_ate:
+   print("I had dinner.")
+else:
+   print("I didn't eat.")
+
