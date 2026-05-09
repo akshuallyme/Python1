1ca9f3891a26d66e7cfbf4d5d0b27c1e03b1d440
Aksh <aksh187k@gmail.com>
2026-05-08 21:01:38 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# For the last time this is my first project.
+print("Give me the goddamn string beans!")
+print("I like fruits!")
+first_name = "Aksh"
+print(f"Hello {first_name}")
+fav_food = "Pizza"
+print(f"{first_name}'s favourite food is {fav_food}.")
