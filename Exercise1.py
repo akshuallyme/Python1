b11585c7cf82abcafe9c8b53ef5ba1c74fe29297
Aksh <aksh187k@gmail.com>
2026-05-09 20:54:18 -0400

    Exercise 1

---
+++ Exercise1.py
@@ -0,0 +1,10 @@
+# Exercise 1 Rectangle are calc
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+height = float(input("Enter the height: "))
+
+area = length * width * height
+
+print(f"The area of the rectangle is {area}cm³.") # for square (Alt+0178) and for cube (Alt+0179) NumLock ON
+
