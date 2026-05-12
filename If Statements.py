3b080e655ed32a713d80d325b60e7eba5044f0a5
Aksh <aksh187k@gmail.com>
2026-05-12 00:44:53 -0400

    If Statements

---
+++ If Statements.py
@@ -0,0 +1,32 @@
+# if = Do some code only if some condition is True
+#      Else do something else
+
+#Ex. 1
+#age = int(input("Enter your age: "))
+#if age >= 100:
+#    print("Bro you too old to sign up!")
+#elif age >= 18:
+#    print("You are now signed up!")
+#elif age < 0:
+#    print("You haven't been born yet!")
+#else:
+#    print("You must be 18+ to sign up!")
+
+
+#Ex. 2
+#response = input("Would you like food? (Y/N): ")
+
+#if response == "Y":
+#    print("Have some food!")
+#else:
+#    print("No food for you!")
+
+
+#Ex. 3
+
+name = input("Enter your name: ")
+
+if name == "":
+    print("You did not type your name.")
+else:
+    print(f"Hello {name}!")
